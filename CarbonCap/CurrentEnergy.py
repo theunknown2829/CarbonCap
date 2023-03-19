bb27a93e03d4287d40bfb4826ef52a590b3eadca
Sakshi Kokane <125692144+theunknown2829@users.noreply.github.com>
2023-03-19 17:33:46 +0000

    Update CurrentEnergy.py

--- CarbonCap/CurrentEnergy.py
+++ CarbonCap/CurrentEnergy.py
@@ -2,22 +2,28 @@ import matplotlib.pyplot as plt
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Rectangle , Color
 
-class Pi(App):
+class Energy(App):
+
     def build(self):
         fig, ax = plt.subplots(figsize=(10,7))
         fig.set_facecolor("#FFF3C8")
         ax.set_facecolor("#FFF3C8")
         layout = BoxLayout(orientation='vertical')
         energy_distribution = [30, 20, 10, 15, 5, 20]
-        labels = ['Refrigerator', 'Washing Machine', 'Heating', 'Lighting', 'Television', 'Other']
+        labels = ['Refrigerator', 'Washing Machine', 'Air Conditioner', 'Lighting', 'Television', 'Other']
         plt.pie(energy_distribution, labels=labels)
+        layout.canvas.before.clear()
+        layout.canvas.before.add(Color(1, 0.95, 0.78, 1))
+        layout.canvas.before.add(Rectangle(pos=layout.pos, size=layout.size))
         plt.title('Energy Distribution Across Household')
         plt.savefig('pie_chart.png')
         image = Image(source='pie_chart.png', allow_stretch=True, keep_ratio=False)
+        total_label = Label(text=f'Total Energy Used: 95 kW', size_hint=(1, None), height=80)
+        layout.add_widget(total_label)
         layout.add_widget(image)
-        total_energy = sum(energy_distribution) / 100 * 10
+
         return layout
-Pi().run()
+
+Energy().run()
